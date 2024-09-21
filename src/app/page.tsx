157d60a33d67051295f7b9906d0271c929dd0555
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-09-21 22:25:39 +0530

    starts

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center pt-20 md:pt-28 px-4">
+      project
+    </div>
+  );
+}
