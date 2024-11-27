24c8bfd9c36af1f2d3735e589a77afcec9d38134
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-11-27 11:31:29 +0530

    minor changes..

--- src/components/section-heading.tsx
+++ src/components/section-heading.tsx
@@ -8,7 +8,7 @@ type SectionheaderProps={
 
 export default function SectionHeading({ children }: SectionheaderProps){
     return(
-        <h2 className="text-3xl font-medium capitalize mb-8 text-center">
+        <h2 className="text-3xl font-medium capitalize mb-6 md:mb-8 text-center">
             {children}
         </h2>
     )
