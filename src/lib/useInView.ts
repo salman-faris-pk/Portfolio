ada0f604575cfe66fb7424ad922b3fa4b86e758d
salmanul faris pk <salmanulfarispk2001@gmail.com>
2025-08-22 12:04:38 +0530

    done k

--- src/lib/useInView.ts
+++ src/lib/useInView.ts
@@ -1,26 +1,31 @@
-"use client"
-import { useEffect } from "react";
+"use client";
+import { useEffect, useLayoutEffect } from "react";
 import { useInView } from "react-intersection-observer";
-import {useActiveSectionContext} from "../containers/active-section"
-import type { SectionName } from "./types"
+import { useActiveSectionContext } from "../containers/active-section";
+import type { SectionName } from "./types";
 import { useRouter } from "next/navigation";
 
+export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
+  const { ref, inView } = useInView({ threshold });
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+  const router = useRouter();
 
-export function useSectionInView(sectionName:SectionName, threshold =0.75){
+  useLayoutEffect(() => {
+    const nav = performance.getEntriesByType("navigation")[0] as | PerformanceNavigationTiming | undefined;
 
-    const {ref,inView}=useInView({ threshold })
+    if (nav?.type === "back_forward") {
+       window.history.replaceState(null, "","/");
+       setActiveSection("#");
+       window.scrollTo({ top: 0, behavior: "instant" });
+    }
+  }, [setActiveSection]);
 
-     const {setActiveSection,timeOfLastClick}=useActiveSectionContext();
-      const router = useRouter();
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection(sectionName);
+      router.replace(sectionName, { scroll: false });
+    }
+  }, [inView, setActiveSection, timeOfLastClick, sectionName, router]);
 
-     useEffect(()=>{
-        if(inView && Date.now() - timeOfLastClick > 1000){
-            setActiveSection(sectionName);
-             router.replace(sectionName, { scroll: false });
-        }
-     },[inView,setActiveSection,timeOfLastClick,sectionName,router]);
-
-     return {
-        ref,
-     };
+  return { ref };
 }
