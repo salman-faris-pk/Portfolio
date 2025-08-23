e67a324553051f5edad8697ee34f68e15b7db6f5
salmanul faris pk <salmanulfarispk2001@gmail.com>
2025-08-23 11:24:57 +0530

    done..

--- src/lib/datas.ts
+++ src/lib/datas.ts
@@ -32,8 +32,8 @@ export const projectData: projectInfo[] =[
       title: "Ecommerce-Shopify",
       description:"Shop effortlessly with our responsive e-commerce website, designed for simplicity on any device.",
       tags:["Next.js","TypeScript","React-vite","Express.js","TailwindCss","MongoDB","Redux-TK","React-Query"],
-      imageUrl: "/ecom.jpeg",
-      Allimg: ["/ecom1.png","/ecom2.png","/ecom3.png","/ecom4.png","/ecom5.png","/ecom6.png","/ecom7.png","/ecom8.png","/ecom9.png","/ecom10.png","/ecom11.png","/ecom12.png"],
+      imageUrl: "/ecom.webp",
+      Allimg: ["/ecom1.webp","/ecom2.webp","/ecom3.webp","/ecom4.webp","/ecom5.webp","/ecom6.webp","/ecom7.webp","/ecom8.webp","/ecom9.webp","/ecom10.webp","/ecom11.webp","/ecom12.webp"],
       Github:"https://github.com/salman-faris-pk/E-Commerce_FullStack",
       preview:"https://ecomerce-frontend-mu.vercel.app"
     },
@@ -41,8 +41,8 @@ export const projectData: projectInfo[] =[
         title: "Resort-Booking",
         description:"Seamlessly responsive and designed for effortless booking, our website delivers a smooth and intuitive user experience",
         tags:["React-vite","Typrscript","TailwindCss","MongoDB","Express.js","stripe","Nodemailer","Cloudinary"],
-        imageUrl: "/rm.png",
-        Allimg: ["/room1.png","/room2.png","/rm3.png","/rm4.png","/rm5.png","/rm6.png","/rm7.png","/rm8.png","/rm9.png","/rm10.png","/rm11.png","/rm12.png","/rm13.png","/rm14.png","/rm15.png"],
+        imageUrl: "/rm.webp",
+        Allimg: ["/room1.webp","/room2.webp","/rm3.webp","/rm4.webp","/rm5.webp","/rm6.webp","/rm7.webp","/rm8.webp","/rm9.webp","/rm10.webp","/rm11.webp","/rm12.webp","/rm13.webp","/rm14.webp","/rm15.webp"],
         Github:"https://github.com/salman-faris-pk/restau_books-mern",
         preview:"https://restau-booking-mern.onrender.com"
       },
@@ -50,8 +50,8 @@ export const projectData: projectInfo[] =[
         title: "Job-finder",
         description:"Find jobs fast with our sleek, responsive platform.Enjoy a smooth, intuitive experience built for success",
         tags:["React-vite","Typescript","TailwindCss","Nest js","MongoDB","prisma","passport auth","OAuth 2.0",],
-        imageUrl: "/jf.jpg",
-        Allimg: ["/jf1.png","/jf2.png","/jf3.png","/jf4.png","/jf5.png","/jf6.png","/jf7.png","/jf8.png","/jf9.png","/jf10.png"],
+        imageUrl: "/jf.webp",
+        Allimg: ["/jf1.webp","/jf2.webp","/jf3.webp","/jf4.webp","/jf5.webp","/jf6.webp","/jf7.webp","/jf8.webp","/jf9.webp","/jf10.webp"],
         Github:"https://github.com/salman-faris-pk/job-finder-react-nest",
         preview:"https://job-finder-pk.netlify.app"
       },
@@ -59,8 +59,8 @@ export const projectData: projectInfo[] =[
         title: "X-Twitter clone",
         description:"A platform to connect, share posts, like content, and follow others seamlessly across all devices, bringing users closer anytime, anywhere.",
         tags:["React-vite","React-query","MongoDB","TailwindCss","Express.js","cloudinary"],
-        imageUrl: "/x.png",
-        Allimg: ["/x1.png","/x2.png","/x3.png","/x4.png","/x5.png","/x6.png","/x7.png","/x8.png","/x9.png"],
+        imageUrl: "/x.webp",
+        Allimg: ["/x1.webp","/x2.webp","/x3.webp","/x4.webp","/x5.webp","/x6.webp","/x7.webp","/x8.webp","/x9.webp"],
         Github:"https://github.com/salman-faris-pk/Twitter_X_MERN_Project",
       },
 ];
